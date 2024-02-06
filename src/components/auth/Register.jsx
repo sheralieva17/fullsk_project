64a4c55d3bdbb05c09352a1f8414529d5ef22f02
Aibek Azizov <aibek.azizov.e@gmail.com>
2024-02-06 20:07:14 +0600

    myChange

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -3,45 +3,45 @@ import "./Register.css";
 
 const Register = () => {
   return (
-    <div className="form">
-      <div className="form-panel two active">
-        <div className="form-header">
+    <div className="form-reg">
+      <div className="form-panel-reg two active">
+        <div className="form-header-reg">
           <h1>Register Account</h1>
         </div>
         <div className="form-content">
           <form>
-            <div className="form-group">
+            <div className="form-group-reg">
               <label htmlFor="email">Email Address</label>
               <input type="email" id="email" name="email" required />
             </div>
-            <div className="form-group">
+            <div className="form-group-reg">
               <label htmlFor="username">Username</label>
               <input type="text" id="username" name="username" required />
             </div>
-            <div className="form-group">
+            <div className="form-group-reg">
               <label htmlFor="password">Password</label>
               <input type="password" id="password" name="password" required />
             </div>
-            <div className="form-group">
+            <div className="form-group-reg">
               <label htmlFor="cpassword">Confirm Password</label>
               <input type="password" id="cpassword" name="cpassword" required />
             </div>
-            <div className="form-group">
+            <div className="form-group-reg">
               <label htmlFor="phone">Phone Number</label>
               <input type="tel" id="phone" name="phone" required />
             </div>
-            <div className="form-group">
+            <div className="form-group-reg">
               <label>Status</label>
               <div>
-                <input type="radio" id="house" name="residenceType" value="house" />
-                <label htmlFor="house">House</label>
+                <input type="radio" id="buyer" name="residenceType" value="buyer" />
+                <label htmlFor="buyer">Buyer</label>
               </div>
               <div>
-                <input type="radio" id="apartment" name="residenceType" value="apartment" />
-                <label htmlFor="apartment">Seller</label>
+                <input type="radio" id="seller" name="residenceType" value="seller" />
+                <label htmlFor="seller">Seller</label>
               </div>
             </div>
-            <div className="form-group">
+            <div className="form-group-reg">
               <button type="submit">Create an account</button>
             </div>
           </form>
